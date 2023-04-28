1c57cd19688e4d1ebb2b12e5156fd592aa64e4f3
goldrago <131658305+goldrago@users.noreply.github.com>
2023-04-28 21:45:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
-// Create the text boxes
-var firstNumber = document.getElementById("firstNumber");
-var secondNumber = document.getElementById("secondNumber");
-
-// Create the drop-down list
-var operation = document.getElementById("operation");
-
-// Create the button
-var calculateButton = document.getElementById("calculateButton");
-
-// Add an event listener to the calculate button
-calculateButton.addEventListener("click", function() {
-
-  // Get the values of the text boxes
-  var firstNumberValue = parseInt(firstNumber.value);
-  var secondNumberValue = parseInt(secondNumber.value);
-
-  // Get the operation that was selected
-  var operationValue = operation.options[operation.selectedIndex].textContent;
-
-  // Validate the first number
-  if (firstNumberValue < 0 || firstNumberValue > 100) {
-    firstNumberError.textContent = "Please enter a number between 0 and 100";
-    return;
-  }
-
-  // Validate the second number
-  if (secondNumberValue < 0 || secondNumberValue > 100) {
-    secondNumberError.textContent = "Please enter a number between 0 and 100";
-    return;
-  }
-
-  // Do the calculation
-  var result;
-  switch (operationValue) {
-    case "Addition":
-      result = firstNumberValue + secondNumberValue;
-      break;
-    case "Subtraction":
-      result = firstNumberValue - secondNumberValue;
-      break;
-    case "Multiplication":
-      result = firstNumberValue * secondNumberValue;
+const calculateBtn = document.getElementById("calculate-btn");
+calculateBtn.addEventListener("click", function() {
+	let num1 = parseInt(document.getElementById("num1").value);
+	let num2 = parseInt(document.getElementById("num2").value);
+	let operation = document.getElementById("operation").value;
+	let result;
+	
+	switch (operation) {
+		case "addition":
+			result = num1 + num2;
+			break;
+		case "subtraction":
+			result = num1 - num2;
+			break;
+		case "multiplication":
+			result = num1 * num2;
+			break;
+		case "division":
+			result = num1 / num2;
+			break;
+	}
+	
+	document.getElementById("result").value = result;
+});
